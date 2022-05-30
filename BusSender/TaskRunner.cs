3f684f0a643d8357f151456a35b01823fba51353
Nick Novotny <ndnovotny@gmail.com>
2022-05-30 00:13:06 -0400

    Test branch (#1)
    * add test consoles
    
    * update log message
    
    * increase time for receive
    
    * change time add more logging
    
    Co-authored-by: Nick Novotny <Nick.Novotny@ddiworld.com>

---
+++ BusSender/TaskRunner.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace BusSender
+{
+    public class TaskRunner
+    {
+        public TaskRunner() { }
+        public async Task Run() {
+            var connectionString = "[ConnectionString]";
+            var topicName = "testtopic1";
+            var client = new ServiceBusClient(connectionString);
+            var sender = client.CreateSender(topicName);
+            var msg = new ServiceBusMessage("TestMessage");
+            await sender.SendMessageAsync(msg);
+            await sender.CloseAsync();
+        }
+    }
+}
